61b4b71f7ad8f13e937f141dcac6874eb2b7bf8c
jhan0121 <jhan0121@gmail.com>
2026-07-03 21:29:36 +0900

    init: create project

---
+++ src/main/java/org/flinter/springresiliencebootvalidation/SpringResilienceBootValidationApplication.java
@@ -0,0 +1,13 @@
+package org.flinter.springresiliencebootvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResilienceBootValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringResilienceBootValidationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/flinter/springresiliencebootvalidation/SpringResilienceBootValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.flinter.springresiliencebootvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResilienceBootValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
